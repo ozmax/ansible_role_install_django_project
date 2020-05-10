5f08b3ac675f0df5262dccdf9c476841e77a090e
Georgios A. Tsakalos <me@ozmax.eu>
2020-05-10 18:22:47 +0300

    make local_settings template extendable

--- templates/local_settings.py
+++ templates/local_settings.py
@@ -5,14 +5,7 @@ ALLOWED_HOSTS = ['{{ domain }}']
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
-# mailchimp
-MAILCHIMP_API_KEY = '{{ mailchimp_api_key }}'
-MAILCHIMP_LIST_ID = '{{ mailchimp_list_id }}'
-MAILCHIMP_DATA_CENTER = '{{ mailchimp_data_center }}'
-
-# email credentials
-EMAIL_HOST_USER = '{{ email_user }}'
-EMAIL_HOST_PASSWORD = '{{ email_password }}'
-
 # sentry
 SENTRY_DSN = '{{ sentry_dsn }}'
+
+{% block extra_vars %}{% endblock %}
